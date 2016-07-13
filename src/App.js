610a77b91a31c6bbbe72a72d692c192e9fddb34e
Jack Franklin <jack@jackfranklin.net>
2016-07-13 11:58:46 +0100

    Set up CSS modules

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './app.css';
 
 export default class App extends React.Component {
   constructor() {
@@ -14,7 +15,7 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className={styles.root}>
         <p>{ this.state.count }</p>
         <button onClick={() => this.increment()}>Increment</button>
       </div>
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+var combineLoaders = require('webpack-combine-loaders');
 
 module.exports = {
   devtool: 'eval',
@@ -21,6 +22,19 @@ module.exports = {
       test: /\.js$/,
       loaders: ['react-hot', 'babel'],
       include: path.join(__dirname, 'src')
+    }, {
+      test: /\.css$/,
+      loader: combineLoaders([
+        {
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader',
+          query: {
+            modules: true,
+            localIdentName: '[name]__[local]___[hash:base64:5]'
+          }
+        }
+      ])
     }]
   }
 };
